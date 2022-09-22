6bdbfe15fd00ccaa06602551ab348cc6377b0130
CarlosNitsuga <42613950+carlosupper@users.noreply.github.com>
2022-09-22 15:15:13 -0500

    upload project

---
+++ UIKitApp/UIKitApp/SignUp/SignUpViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignUpViewController.swift
+//  UIKitApp
+//
+//  Created by Laura Anguiano on 15/09/22.
+//
+
+import UIKit
+
+final class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func didTapSignUp(_ sender: Any) {
+        let homeView =  self.storyboard?.instantiateViewController(withIdentifier: "welcomevc") as! WelcomeViewController
+        self.present(homeView, animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapSignIn(_ sender: Any) {
+        
+    }
+}
+
+// MARK: - Static Methods
+extension SignUpViewController {
+    static func getViewController() -> SignUpViewController {
+        let storyboard = UIStoryboard(name: "SignUp", bundle: nil)
+        guard let signUpViewController = storyboard.instantiateViewController(withIdentifier: "SignUpViewController") as? SignUpViewController else {
+            fatalError("ViewController must be of type SignUpViewController")
+        }
+        return signUpViewController
+    }
+}
